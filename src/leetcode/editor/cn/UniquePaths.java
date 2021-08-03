76a6d4bf8a1c495481797cdc2d761adda308cbf2
Justify-meaning <Hcsjustify@outlook.com>
2021-08-03 20:13:30 +0800

    leetcode

--- src/leetcode/editor/cn/UniquePaths.java
+++ src/leetcode/editor/cn/UniquePaths.java
@@ -36,9 +36,49 @@ public class UniquePaths {
  }
  //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+     /**
+      * 我们用 f(i, j)表示从左上角走到 (i, j) 的路径数量，其中 i 和 j 的范围分别是 [0, m) 和 [0, n)。
+      * 由于我们每一步只能从向下或者向右移动一步，因此要想走到 (i, j)(i,j)，如果向下走一步，那么会从 (i-1, j) 走过来；
+      * 如果向右走一步，那么会从 (i, j-1) 走过来。因此我们可以写出动态规划转移方程：
+      * f(i, j) = f(i-1, j) + f(i, j-1)
+      * @param m
+      * @param n
+      * @return
+      */
+
     public int uniquePaths(int m, int n) {
+        int[][] result = new int[m][n];
+        for (int i = 0; i < m; i++){
+        //    走到第一列的任意位置路径数量都只有一条
+            result[i][0] = 1;
+        }
+
+        for (int j = 0; j < n; j++){
+        //    走到第一行的任意位置路径数量都只有一条
+            result[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++){
+            for (int j = 1; j < n; j++){
+                //走到（i,j）点相当于（i-1,j）向下走一步和（i，j-1）向右走一步
+                result[i][j] = result[i - 1][j] + result[i][j - 1];
+            }
+        }
+        //返回动态规划数组右下角的值，就是统计得到的全部路径数量
+        return result[m - 1][n - 1];
 
     }
+
+     //数学排列组合的计算方法
+     public int uniquePaths2(int m, int n) {
+         //只跟第几行第几列有关，从m+n-2步中抽出m-1步
+         long ans=1;
+         for(int i=0;i<Math.min(m-1,n-1);i++){
+             ans*=m+n-2-i;
+             ans/=i+1;
+         }
+         return (int)ans;
+     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
